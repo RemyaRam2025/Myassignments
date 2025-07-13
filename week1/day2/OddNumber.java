ba3097a3d92a13a2de5618eddff45955760377d4
RemyaRam2025 <remya.aquarius@gmail.com>
2025-07-13 19:04:20 +0530

    Add files via upload

---
+++ week1/day2/OddNumber.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		//Program to check if the number is odd or not
+		int num = 5;
+		if (num%2!=0)		
+			System.out.println("Number is odd");
+		else
+			System.out.println("Number is even");
+		
+		
+
+	}
+}
+
+
