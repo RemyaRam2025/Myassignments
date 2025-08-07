0d63a761f7ac2e166750003484cae4a897d949d3
RemyaRam2025 <remya.aquarius@gmail.com>
2025-08-07 08:20:57 +0530

    Add files via upload

---
+++ week5/day2/CreateLead.java
@@ -0,0 +1,25 @@
+package week5.day2;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class CreateLead extends BaseClass {
+	
+	@BeforeTest
+	public void setValues() {
+		filename="CreateLead";
+
+	}
+
+	@Test(dataProvider = "fetchData")
+	public void createLead(String companyName, String firstName, String lastName) {
+
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys(companyName);
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys(firstName);
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys(lastName);
+		driver.findElement(By.name("submitButton")).click();
+
+	}
+}
